4c79703aca996247c71683e5f2aced5d45ffe88f
Nimisha Asthagiri <nasthagiri@edx.org>
2015-04-09 16:44:18 -0400

    fixup.

--- cms/djangoapps/contentstore/admin.py
+++ cms/djangoapps/contentstore/admin.py
@@ -5,7 +5,7 @@ Admin site bindings for contentstore
 from django.contrib import admin
 
 from config_models.admin import ConfigurationModelAdmin
-from contentstore.models import VideoUploadConfig
+from contentstore.models import VideoUploadConfig, MobileNotificationsConfig
 
 admin.site.register(VideoUploadConfig, ConfigurationModelAdmin)
 admin.site.register(MobileNotificationsConfig, ConfigurationModelAdmin)
